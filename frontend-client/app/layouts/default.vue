7a82a28bfa59aa7ace90fcab10131340dc44220d
Ailwyn0822 <asd66151200@gmail.com>
2026-01-28 19:09:31 +0800

    feat:將無效按鈕替換成登入按鈕

--- frontend-client/app/layouts/default.vue
+++ frontend-client/app/layouts/default.vue
@@ -32,12 +32,11 @@
                             class="w-full pl-10 pr-4 py-3 rounded-xl border-2 border-dashed border-gray-400 focus:border-[#1c180d] focus:ring-0 text-sm font-medium placeholder-gray-400 shadow-sm"
                             placeholder="Search for toys, books..." type="text" />
                     </div>
-                    <button
-                        class="size-10 bg-accent-purple rounded-full border-2 border-[#1c180d] flex items-center justify-center shadow-[2px_2px_0px_#1c180d] hover:translate-y-0.5 hover:shadow-none transition-all">
-                        <div class="size-full rounded-full bg-cover bg-center" data-alt="User profile picture"
-                            style="background-image: url('https://lh3.googleusercontent.com/aida-public/AB6AXuBVTrN5eP8gddcGv3gNQ4e7vvxUhjjaUpzVICx4u-gJwsma-Kl9Ws97LOwtsAn53AZgHfiAKG4kbIbKCqKt4r7-IYl2QBBWfClDNHNQ0jnOpNnq9YHw0-3CoTmABdq3VqtcZ1cJUvOWPnv7M_X_MmKdbq67CVO79muQekcpL3Ys3_GGw_Zrk726FsAA35wRHVKQowWsHJsm7MBN6ZkJEUJfHuaynrhEeYBoyLQDMK35yQJsm2Yq3lcut1432TRfnqzsLOengwIpgRHO');">
-                        </div>
-                    </button>
+                    <NuxtLink to="/login"
+                        class="bg-primary px-5 py-2 rounded-lg border-2 border-[#1c180d] text-[#1c180d] font-bold text-sm shadow-[2px_2px_0px_#1c180d] hover:translate-y-0.5 hover:shadow-none transition-all flex items-center gap-2">
+                        <span class="material-symbols-outlined text-lg">login</span>
+                        Login
+                    </NuxtLink>
                 </div>
             </div>
         </header>
