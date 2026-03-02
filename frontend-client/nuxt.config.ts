940188e4b9ac0fea204a9610718e80d343966c9c
Ailwyn0822 <asd66151200@gmail.com>
2026-03-02 18:19:51 +0800

    Revert "fix:SWR問題"
    This reverts commit 886cb67a51dbe76b97fb6aacc40d6fc528b12470.

--- frontend-client/nuxt.config.ts
+++ frontend-client/nuxt.config.ts
@@ -38,7 +38,7 @@ export default defineNuxtConfig({
     langDir: './language',
     defaultLocale: 'zh-TW',
     detectBrowserLanguage: {
-      useCookie: false,
+      useCookie: true,
       cookieKey: 'smart_market_lang',
       redirectOn: 'root'
     }
